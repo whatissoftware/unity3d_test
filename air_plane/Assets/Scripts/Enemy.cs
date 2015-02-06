db78c62c764f4bc08bb16cf570fb959aee5c47c2
whatissoftware <whatissoftware@hotmail.com>
2015-02-06 18:13:56 +0800

    first add
    juset finish palyer rocket enemy

---
+++ air_plane/Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public float m_speed = 0.1f;
+    protected float m_rotSpeed = 30;
+    protected float m_changeDirectionTimer = 1.5f;
+    protected Transform m_transform;
+
+    public float m_life = 1;
+
+	// Use this for initialization
+	void Start () {
+        this.m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        UpdateMove();
+	}
+
+    protected void UpdateMove()
+    {
+        m_changeDirectionTimer -= Time.deltaTime;
+        if (m_changeDirectionTimer <= 0)
+        {
+            m_changeDirectionTimer = 3;
+
+            m_rotSpeed = -m_rotSpeed;
+        }
+
+        m_transform.Rotate(Vector3.up, m_rotSpeed * Time.deltaTime, Space.World);
+
+        m_transform.Translate(new Vector3(0, 0, -m_speed * Time.deltaTime));
+
+    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.CompareTo("PlayerRocket") == 0)
+        {
+            Rocket rocket = other.GetComponent<Rocket>();
+            if (rocket != null)
+            {
+                m_life -= rocket.m_power;   
+            }
+        }
+        else if (other.tag.CompareTo("Player") == 0)
+        {
+            m_life = 0;
+        }
+
+
+        if (m_life <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+
+}
---
+++ air_plane/Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("MyGame/Player")]
+public class Player : MonoBehaviour {
+
+
+    public float m_speed = 1;
+
+
+    protected Transform m_transform;
+
+    public Transform m_rocket;
+    
+    protected float m_rocketRate = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float movex = 0;
+        float movez = 0;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            movez -= m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            movez += m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movex += m_speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movex -= m_speed * Time.deltaTime;
+        }
+
+        m_transform.Translate(new Vector3(movex, 0, movez));
+
+        m_rocketRate -= Time.deltaTime;
+
+        if(m_rocketRate<0)
+        {
+            m_rocketRate = 0.1f;
+
+            if (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+            {
+                Instantiate(m_rocket, m_transform.position, m_transform.rotation);
+            
+            }
+
+        }
+
+	}
+}
